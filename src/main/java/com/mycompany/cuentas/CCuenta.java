e99fae82f858963d823440c4747597b72e7dd252
iriad <iriad@LAPTOP-ON0KDJ46>
2023-03-04 14:40:41 +0100

    Commit NetBeans

---
+++ src/main/java/com/mycompany/cuentas/CCuenta.java
@@ -0,0 +1,110 @@
+package com.mycompany.cuentas;
+
+public class CCuenta {
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInteres;
+
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+    /**
+     * Obtiene el saldo
+     * @return getsaldo
+     */
+    
+    public double estado()
+    {
+        return getSaldo();
+    }
+    /**
+     * Ingresar la cantidad correspondiente.
+     * @param cantidad indica la cantidada a ingresar
+     * @throws Exception  Se captura una excepción
+     */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+    /**
+     * Retirar la cantidad correspondiente.
+     * @param cantidad indica la cantidad a retirar
+     * @throws Exception Se captura una excepción
+     */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+
+    /**
+     * @return nombre 
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set 
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInteres
+     */
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+
+    /**
+     * @param tipoInteres the tipoInteres to set
+     */
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
+    }
+}
+
---
+++ src/main/java/com/mycompany/cuentas/Main.java
@@ -0,0 +1,44 @@
+package com.mycompany.cuentas;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new CCuenta("Antonio Lopez","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
+
+        try {
+            operativa_cuenta(cuenta1, false, 2300);
+            System.out.println("Retiro en cuenta" );
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }         
+         
+        try {
+            operativa_cuenta(cuenta1, true, 695 );
+            System.out.println("Ingreso en cuenta");
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+    /**
+     * 
+     * @param cuenta1
+     * @param ingresar
+     * @param cantidad
+     * @throws Exception 
+     */
+    public static void operativa_cuenta(CCuenta cuenta1, boolean ingresar, float cantidad) throws Exception {
+        
+        if(ingresar == true)
+            cuenta1.ingresar(cantidad);
+       else
+            cuenta1.retirar(cantidad);
+        
+    }
+}
+
+    
